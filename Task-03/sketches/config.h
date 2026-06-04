bdb50cb5faa666f1d1afb1c37189a022acb2d475
jyothishss007-beep <jyothishss007@gmail.com>
2026-06-04 12:33:46 +0530

    Create config.h

---
+++ Task-03/sketches/config.h
@@ -0,0 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Pin definitions
+const int BUTTON_PIN = 2;
+const int LED_PIN    = 7;
+const int RELAY_PIN  = 8;
+
+// Relay settings
+const bool RELAY_ACTIVE_LOW = true;
+
+// Serial settings
+const long SERIAL_BAUDRATE = 9600;
+
+#endif
